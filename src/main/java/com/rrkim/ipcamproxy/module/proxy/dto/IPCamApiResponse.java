476bc7108668b5ed0add0eab40389d4944bff8dd
Ryangrae Kim <dev@rrkim.com>
2024-05-21 13:13:13 +0900

    fix: API 요청 JSON 으로 변경

---
+++ src/main/java/com/rrkim/ipcamproxy/module/proxy/dto/IPCamApiResponse.java
@@ -0,0 +1,11 @@
+package com.rrkim.ipcamproxy.module.proxy.dto;
+
+import lombok.Data;
+
+import java.util.Map;
+
+@Data
+public class IPCamApiResponse {
+
+    Map<String, Object> data;
+}
---
+++ src/main/java/com/rrkim/ipcamproxy/module/proxy/dto/SecureKeyRequestDto.java
@@ -0,0 +1,9 @@
+package com.rrkim.ipcamproxy.module.proxy.dto;
+
+import lombok.Data;
+
+@Data
+public class SecureKeyRequestDto {
+
+    String deviceId;
+}
